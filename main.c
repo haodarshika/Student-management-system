71c6aa7ac660ff2216301acce28382ad95f63656
haodarshika <darshikaoshani@gmail.com>
2026-06-01 08:56:22 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+   int id[20];
+   char name[50];
+   float marks;
+
+   printf("Enter Student ID:");
+   scanf("%s",id);
+
+   printf("Enter Student Name:");
+   scanf("%s",name);
+
+   printf("Enter Student Marks:");
+   scanf("%f",&marks);
+
+   printf("\n---Student Details---\n");
+   printf("ID:%s\n",id);
+   printf("Name:%s\n",name);
+   printf("Marks:%.2f\n",marks);
+   return 0;
+}
+
+
