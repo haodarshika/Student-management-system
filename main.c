5c774ff9e8dbb752eb39c56650c17b90636664bd
kaveeshalakshani <deerasinghekaveesha@gmail.com>
2026-06-01 09:12:26 +0530

    Update main.c

--- main.c
+++ main.c
@@ -4,7 +4,8 @@ int main(){
    int id[20];
    char name[50];
    float marks;
-
+   
+//student input details
    printf("Enter Student ID:");
    scanf("%s",id);
 
@@ -13,7 +14,7 @@ int main(){
 
    printf("Enter Student Marks:");
    scanf("%f",&marks);
-
+//output details
    printf("\n---Student Details---\n");
    printf("ID:%s\n",id);
    printf("Name:%s\n",name);
